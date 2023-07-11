f63e9f33034c50f2ae4dc69adfdbe4a58a866a39
tebazele <112727701+tebazele@users.noreply.github.com>
2023-07-11 17:49:25 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// 💻 Create three arrays of strings, one called "randomBodyParts", one called "randomAdjectives", and one called "randomWords".
+// Put at least 10 strings in each
+
+let randomBodyParts = [];
+let randomeAdjectives = [];
+let randomWords = [];
+
+/*
+💻 Write your generateInsult() function here
+*/
+function generateInsult() {
+
+  document.getElementById("insult-spot").innerText = "Unwritten Insult"
+}
+
+// 🧩 Open the index.html file and add an onclick to the pig image that calls the generateInsult() function
