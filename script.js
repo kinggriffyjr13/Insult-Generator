50da662c5801e31804067d4748d180896eae47cb
kinggriffyjr13 <139810564+kinggriffyjr13@users.noreply.github.com>
2023-07-18 20:32:07 +0000

    mean cute pig

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 // 💻 Create three arrays of strings, one called "randomBodyParts", one called "randomAdjectives", and one called "randomWords".
 // Put at least 10 strings in each
 
-let randomBodyParts = [];
-let randomeAdjectives = [];
-let randomWords = [];
+let randomBodyParts = ["head",
+"ears",
+"nose",
+"tail",
+"hands",
+"feet",
+"chest",
+"neck",
+"back",
+"toenails",
+"belly button",
+"head",
+"nose hair"]
+let randomAdjectives = ["greasy",
+"smelly",
+"ugly",
+"purple",
+"FAT",
+"trash",
+];
+let randomWords = [
+    "pig", "armour", "jumper cabel ", "optims primes exost pipe", "bag", "white van" 
+];
 
 /*
 💻 Write the code to generate the insult here
 */
+let bodyPartIndex = Math.floor(Math.random()*randomBodyParts.length)
+let adjectiveIndex = Math.floor(Math.random()*randomAdjectives.length)
+let wordIndex = Math.floor(Math.random()*randomWords.length)
+
+let bodyPart = randomBodyParts[bodyPartIndex]
+let adjective = randomAdjectives[adjectiveIndex]
+let word = randomWords[wordIndex]
+
+let insult = "Your " + bodyPart + " is like a " + adjective + " " + word + "!"
 
 // Change "Unwritten Insult" to your insult variable
 document.getElementById("insult-spot").innerText = "Unwritten Insult"
